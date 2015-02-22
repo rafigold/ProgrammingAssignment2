474045294704b50a428d73d5e244dcc5d213b147
rafigold <rafigold@users.noreply.github.com>
2015-02-22 19:47:25 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes a square matrix and returns the inverse. The matrix is also cached to the environment
+## which allows it and the inverse (if it has been previously calculated) to be referenced without needing to be recalculated.
 
-## Write a short comment describing this function
+
+## The variable x is used to hold the matrix to be inverted and m is used to hold the inverse.
+## m gets reset to NULL if input matrix changes
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
 
+	set <- function(y){
+		x <<- y # saves the matrix so that cacheSolve can check whether or not it has been updated
+		m <<- NULL
+	}
+	get <- function() x
+	setmatrix <- function(solve) m <<- solve
+	getmatrix <- function() m
+
+
+	# create a list of 4 functions defined above 
+ 
+	list(set = set, get = get,
+	setmatrix = setmatrix,
+ 	getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
-
+	
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Returns a matrix that is the inverse of 'x'
+
+	m <- x$getmatrix()
+	
+	# check if inverse has been calculated and matrix remains unchanged
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+		}
+	
+	matrix <- x$get()
+
+	m <- solve(matrix, ...) # solve calculates the inverse of a square invertible matrix 
+
+	x$setmatrix(m) # set x = m for checking next time the function is run
+
+	m # return m as the inverse matrix solution
 }
+
